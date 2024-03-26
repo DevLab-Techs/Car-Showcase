fce7c1cb1effd294d1a48f068c0587087ae6a98f
E-YoussefAhmed <ya23122002@gmail.com>
2024-03-27 01:54:01 +0200

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,39 @@
+import { CustomFilter, Hero, SearchBar } from "@/components";
+import { AllCars } from "@/components/AllCars";
+import { Loader } from "@/components/Loader";
+import { fuels, yearsOfProduction } from "@/constants";
+import { FilterProps } from "@/types";
+import { Suspense } from "react";
+
+export default function Home({ searchParams }: { searchParams: FilterProps }) {
+  return (
+    <main className="overflow-hidden">
+      <Hero />
+      <div className="mt-12 padding-x padding-y max-width" id="discover">
+        <div className="home__text-container">
+          <h1 className="text-4xl font-extrabold">Car Catalogue</h1>
+          <p>Explore the cars you might like</p>
+        </div>
+        <div className="home__filters">
+          <SearchBar />
+          <div className="home__filter-container">
+            <CustomFilter title="fuel" options={fuels} />
+            <CustomFilter title="year" options={yearsOfProduction} />
+          </div>
+        </div>
+        <Suspense
+          key={
+            searchParams.fuel +
+            searchParams.limit +
+            searchParams.make +
+            searchParams.make +
+            searchParams.year
+          }
+          fallback={<Loader />}
+        >
+          <AllCars {...searchParams} />
+        </Suspense>
+      </div>
+    </main>
+  );
+}
---
+++ components/Loader.tsx
@@ -0,0 +1,5 @@
+export const Loader = () => {
+  return (
+    <div className="text-4xl md:text-6xl text-center my-20">Loading...</div>
+  );
+};
